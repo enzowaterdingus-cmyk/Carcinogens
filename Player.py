a58dd64c2679aab5d7df22166d8518d3092d4296
enzowaterdingus-cmyk <enzowaterdingus@gmail.com>
2026-06-16 13:07:46 -0700

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,6 +1,7 @@
 import pygame
 from dynamicPoint import *
-from constants import *
+#from constants import *
+import constants
 class Player:
     def __init__(self, x, y):
         self.x = x
@@ -19,15 +20,15 @@ class Player:
         pygame.draw.line(screen, "red", (self.dev_shape.right, self.dev_shape.top), (self.dev_shape.left, self.dev_shape.bottom), 2)
 
     def update_controls(self):
-        global DIRECTION
-        global SCROLL_X
-        global SCROLL_Y
+        
+        #global SCROLL_X
+        #global SCROLL_Y
     
         self.x += self.vx
         self.y += self.vy
         self.point.x = self.x
         self.point.y = self.y
-        self.point = self.point.translate(-SCROLL_X, -SCROLL_Y)
+        self.point = self.point.translate(-constants.SCROLL_X, -constants.SCROLL_Y)
         self.dev_shape = pygame.Rect(self.point.x-20, self.point.y-30, 40, 60)
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
@@ -45,5 +46,5 @@ class Player:
         self.vx *= 0.9
         self.vy *= 0.9
 
-        SCROLL_X += ((self.x-CENTER_X) - SCROLL_X)/4
-        SCROLL_Y += ((self.y-CENTER_Y) - SCROLL_Y)/4
+        constants.SCROLL_X += ((self.x-constants.CENTER_X) - constants.SCROLL_X)/4
+        constants.SCROLL_Y += ((self.y-constants.CENTER_Y) - constants.SCROLL_Y)/4
